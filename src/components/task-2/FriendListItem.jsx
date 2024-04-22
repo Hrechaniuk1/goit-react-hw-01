8ed21b0a51e9c605a2747bed7ce6208c97321034
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-04-22 18:56:33 +0300

    task 2 done

--- src/components/task-2/FriendListItem.jsx
+++ src/components/task-2/FriendListItem.jsx
@@ -1,9 +1,16 @@
 export default function FriendListItem({ avatar, name, isOnline }) {
+    const status = isOnline ? "Online" : "Offline"
+    const classList = ["friend-status"]
+    if (isOnline) {
+        classList.push("On")
+    } else {
+        classList.push("Off")
+    }
     return (
         <li className="friend-list-item">
             <img className="friend-img" src={avatar} alt={name} />
             <h2 className="friend-name">{name}</h2>
-            <p className="friend-status">{isOnline}</p>
+            <p className={classList.join(" ")}>{status}</p>
         </li>
     )
 }
